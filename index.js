7358edeced866056e9ef4f8a8e99fb764e493eb7
ChenLili96 <32363108+ChenLili96@users.noreply.github.com>
2018-04-26 20:06:35 +0100

    RR
    RR

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
 Backendless.initApp("0B5B881C-AB5A-D7C5-FF52-297088A1FC00","9507AC93-13CF-F45F-FFA2-0DEF6F8E8F00");
 
 $(document).on("pageshow","#todopage", onPageShow);
-
 function onPageShow() {
 	console.log("page shown");
     
 	$("#taskList").empty();
-      
+	
 	//run a query
 	Backendless.Data.of( "Tasks" ).find().then( processResults).catch(error);
     
     $(document).on("click", "#addTaskButton", onAddTask);
-
-	
-
 
 } 
 
@@ -23,17 +19,20 @@ function onAddTask() {
         
         var newTask = {};
         newTask.Task = tasktext;
-    
-        Backendless.Data.of("Tasks").save(newTask).then(saved).catch(error);
-
+        Backendless.Data.of("Tasks").save(newTask).then(saved).catch(error);    
+        onPageShow();
+        document.getElementById("addTaskText").value="";
+   
 }
 
 function saved(savedTask) { 
       console.log( "new Contact instance has been saved" + savedTask);
+
 }
 
+
 function  processResults(tasks) {
-    alert(tasks[1].Task)
+    //alert(tasks[1].Task)
     
     //add each tasks
     for (var i = 0; i < tasks.length; i++) { 
@@ -50,5 +49,3 @@ function error(err) {
     alert(err);
 }
 
-
-  
